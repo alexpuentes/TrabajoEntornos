8e4d471ab585a8605bdc18f3ba865f2b05ca45bc
Puentes <103957794+alexpuentes@users.noreply.github.com>
2022-05-09 12:35:58 +0200

    Add files via upload
    Primer upload del trabajo.

---
+++ tema20_ejercicio5.py
@@ -0,0 +1,71 @@
+def mediaAritmetica(n1,n2):
+    return (n1 + n2)/2
+ 
+    """
+    Toma dos números como entrada y devuelve el promedio de esos dos números
+    
+    @param n1 el primer numero
+    @param n2 el segundo numero
+    
+    @return El promedio de los dos números.
+    """
+ 
+   
+    
+def hipotenusa (cA, cB):
+    return (cA**2)+(cB**2)
+
+    """
+    Toma dos números como entrada y devuelve el producto de los dos números multiplicado por 1.21
+    
+    @param cantidad La cantidad de productos que desea comprar.
+    @param precioProducto El precio del producto
+    
+    @return el resultado de la multiplicación de los parámetros.
+    """
+    
+    
+    
+def calcularIVA(cantidad, precioProducto):
+    return (cantidad*precioProducto)*1.21
+
+    """
+    Toma dos números como entrada y devuelve el producto de esos números multiplicado por 1.21
+    
+    @param cantidad La cantidad de productos que desea comprar.
+    @param precioProducto El precio del producto
+    
+    @return el resultado de la multiplicación de los parámetros.
+    """
+
+
+
+
+def sumatorio (numero):
+    
+    for i in range (0, numero):
+        numero= numero+i
+    return numero
+
+    """
+    Suma todos los números del 0 al número que le pasas
+    
+    @param numero El número que se sumará a la suma.
+    
+    @return La suma de todos los números desde 0 hasta el número dado.
+    """
+
+
+def factorial (numero):
+    
+    for i in range (1, numero):
+        numero= numero*i
+    return numero
+
+    """
+    Calcula el factorial de un número.
+    
+    @param numero El número del que quieres encontrar el factorial.
+    
+    @return El factorial del número.
+    """
